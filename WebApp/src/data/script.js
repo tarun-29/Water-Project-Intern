5be72e494ed5f41f1bf6b7c743db895cde9b451a
tarun-29 <coe17b031@iiitdm.ac.in>
2020-09-20 00:39:37 +0530

    Keeras json model

--- WebApp/src/data/script.js
+++ WebApp/src/data/script.js
@@ -1,1 +1,17 @@
 console.log('Hello TensorFlow');
+
+async function getData() {
+    const waterDataReq = await fetch('https://raw.githubusercontent.com/tarun-29/Water-Project-Intern/master/water.json');  
+    console.log("ofjgrhdsngjvn")
+    const waterData = await waterDataReq.json();  
+    console.log(waterData)
+    const cleaned = waterData.map(w => ({
+      mpg: w.Miles_per_Gallon,
+      horsepower: car.Horsepower,
+    }))
+    .filter(car => (car.mpg != null && car.horsepower != null));
+    
+    return cleaned;
+  }
+
+  getData()
