35393f8f1dc4e28a7d2ed3b7d8d69f4449755cbd
tarun-29 <coe17b031@iiitdm.ac.in>
2020-09-20 23:47:44 +0530

    water.json file and tensorflow json model

--- WebApp/src/data/script.js
+++
@@ -1,8 +0,0 @@
-import * as tf from "@tensorflow/tfjs"
-
-const modelUrl =  "https://raw.githubusercontent.com/tarun-29/Water-Project-Intern/master/tfjs/model.json"
-
-const model = await tf.loadLayersModel(modelUrl);
-
-
-const prediction = model.predict(example);
